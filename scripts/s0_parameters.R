029fa1a27f13baca7b35a59a178446e2a118182d
lecrabe <remi.dannunzio@gmail.com>
2019-06-11 09:41:32 +0700

    setup

---
+++ scripts/s0_parameters.R
@@ -0,0 +1,51 @@
+####################################################################################################
+####################################################################################################
+## Set environment variables
+## Contact remi.dannunzio@fao.org 
+## 2019/06/11
+####################################################################################################
+####################################################################################################
+####################################################################################################
+
+### Read all external files with TEXT as TEXT
+options(stringsAsFactors = FALSE)
+
+### Create a function that checks if a package is installed and installs it otherwise
+packages <- function(x){
+  x <- as.character(match.call()[[2]])
+  if (!require(x,character.only=TRUE)){
+    install.packages(pkgs=x,repos="http://cran.r-project.org")
+    require(x,character.only=TRUE)
+  }
+}
+
+### Install (if necessary) two missing packages in your local SEPAL environment
+packages(Hmisc)
+packages(lubridate)
+
+### Load necessary packages
+packages(raster)
+packages(rgeos)
+packages(rgdal)
+
+## Set the working directory
+rootdir       <- "~/ws_idn_20190611/"
+
+## Set the country code
+countrycode <- "IDN"
+
+## Go to the root directory
+setwd(rootdir)
+rootdir  <- paste0(getwd(),"/")
+username <- unlist(strsplit(rootdir,"/"))[3]
+
+scriptdir <- paste0(rootdir,"scripts/")
+doc_dir   <- paste0(rootdir,"docs/")
+data_dir  <- paste0(rootdir,"data/")
+gadm_dir  <- paste0(rootdir,"data/gadm/")
+tile_dir  <- paste0(rootdir,"data/tiling/")
+aoi_dir   <- paste0(rootdir,"data/aoi/")
+
+dir.create(gadm_dir,showWarnings = F)
+dir.create(tile_dir,showWarnings = F)
+
---
+++ scripts/s1_create_tiling_system.R
@@ -0,0 +1,126 @@
+####################################################################################################
+####################################################################################################
+## Read, manipulate and write spatial vector data, Get GADM data
+## Contact remi.dannunzio@fao.org 
+## 2018/08/22
+####################################################################################################
+####################################################################################################
+
+
+####################################################################################################
+################################### PART I: GET GADM DATA
+####################################################################################################
+
+## Get the list of countries from getData: "getData"
+(gadm_list  <- data.frame(raster::getData('ISO3')))
+
+## Get GADM data, check object properties
+country         <- raster::getData('GADM',path=gadm_dir , country= countrycode, level=1)
+
+summary(country)
+extent(country)
+proj4string(country)
+
+## Display the SPDF
+plot(country)
+
+##  Export the SpatialPolygonDataFrame as a ESRI Shapefile
+writeOGR(country,
+         paste0(gadm_dir,"gadm_",countrycode,"_l1.shp"),
+         paste0("gadm_",countrycode,"_l1"),
+         "ESRI Shapefile",
+         overwrite_layer = T)
+
+
+####################################################################################################
+################################### PART II: CREATE A TILING OVER AN AREA OF INTEREST
+####################################################################################################
+
+### What grid size do we need ? 
+grid_size <- 20000          ## in meters
+grid_deg  <- grid_size/111320 ## in degree
+
+generate_grid <- function(aoi,size){
+### Create a set of regular SpatialPoints on the extent of the created polygons  
+sqr <- SpatialPoints(makegrid(aoi,offset=c(0.5,0.5),cellsize = size))
+
+### Convert points to a square grid
+grid <- points2grid(sqr)
+
+### Convert the grid to SpatialPolygonDataFrame
+SpP_grd <- as.SpatialPolygons.GridTopology(grid)
+
+sqr_df <- SpatialPolygonsDataFrame(Sr=SpP_grd,
+                                   data=data.frame(rep(1,length(SpP_grd))),
+                                   match.ID=F)
+
+### Assign the right projection
+proj4string(sqr_df) <- proj4string(aoi)
+sqr_df
+}
+
+sqr_df <- generate_grid(country,grid_deg)
+
+nrow(sqr_df)
+
+### Select a vector from location of another vector
+aoi <- readOGR(paste0(aoi_dir,"gwl_mef_example.shp"))
+
+### Select a vector from location of another vector
+sqr_df_selected <- sqr_df[aoi,]
+nrow(sqr_df_selected)
+
+### Plot the results
+plot(sqr_df_selected)
+plot(aoi,add=T,border="blue")
+plot(country,add=T,border="green")
+
+### Give the output a decent name, with unique ID
+names(sqr_df_selected@data) <- "tileID" 
+sqr_df_selected@data$tileID <- row(sqr_df_selected@data)[,1]
+
+
+### Export ONE TILE as KML
+export_name <- paste0("one_tile_",countrycode)
+one_tile <- sqr_df_selected[sample(1:nrow(sqr_df_selected@data),1),]
+
+plot(one_tile,add=T,col="blue")
+
+writeOGR(obj=   one_tile,
+         dsn=   paste(tile_dir,export_name,".kml",sep=""),
+         layer= export_name,
+         driver = "KML",
+         overwrite_layer = T)
+
+# ### Distribute samples among users
+# df <- sqr_df_selected@data
+# 
+# users <- read.csv(paste0(doc_dir,"participants_workshop_20190611.csv"))
+# 
+# df    <- data.frame(cbind(df$tileID,users$username))
+# 
+# names(df) <- c("tileID","username")
+# 
+# df$tileID <- as.numeric(df$tileID)
+# 
+# ### Create a final subset corresponding to your username
+# my_tiles <- tiles[tiles$tileID %in% df[df$username == username,"tileID"],]
+# plot(my_tiles,add=T,col="yellow")
+# 
+# ### Export the final subset
+# export_name <- paste0("tiles_",username)
+# 
+# writeOGR(obj=my_tiles,
+#          dsn=paste(tile_dir,export_name,".kml",sep=""),
+#          layer= export_name,
+#          driver = "KML",
+#          overwrite_layer = T)
+
+### Export ALL TILES as KML
+export_name <- paste0("tiling_system_aoi")
+
+writeOGR(obj=sqr_df_selected,
+         dsn=paste(tile_dir,export_name,".kml",sep=""),
+         layer= export_name,
+         driver = "KML",
+         overwrite_layer = T)
